b309a431de6cdec9a0fa33126c55cbe112afeb84
btnwilson <138797732+btnwilson@users.noreply.github.com>
2024-02-14 13:34:55 -0500

    Update Week5Simulate.py

--- Week5Simulate.py
+++ Week5Simulate.py
@@ -5,12 +5,12 @@ Created on Mon Feb 12 15:41:47 2024
 @author: bentn
 """
 
-import Week5AnabolicCell as AC
+import Week5Cell as AC
 import numpy as np 
 import random as r
 import matplotlib.pyplot as plt
 
-def move(cell, array_size):
+def move_random(cell, array_size):
     change_row = r.randint(-1,1)
     change_column = r.randint(-1,1)
     row_position = cell.Get_Row()
@@ -29,15 +29,19 @@ def move(cell, array_size):
     cell.Set_Column(new_column)
     return new_row, new_column
 
+def move_durotaxis(cell, array_size):
+    pass
 
 array_size = 100
-grid = np.zeros((array_size,array_size))
-vmin, vmax = 0, 5
+grid = np.full((array_size,array_size), .5)
+cell_location = np.full((array_size, array_size), np.nan)
+vmin, vmax = 0, 1
 fig, ax = plt.subplots()
 
 
-heatmap = ax.imshow(grid, cmap='hot', interpolation='nearest', vmin=vmin, vmax=vmax)
-plt.colorbar(heatmap)
+tissue = ax.imshow(grid, cmap='hot', interpolation='nearest', vmin=vmin, vmax=vmax)
+cell = ax.imshow(cell_location, cmap='winter', interpolation='nearest', vmin=vmin, vmax=vmax)
+plt.colorbar(tissue)
 
 
 
@@ -46,24 +50,40 @@ catabolic_cells = {}
 
 num_cells = 20
 for i in range(num_cells):
-    anabolic_cells[f"A_Cell_{i+1}"] = AC.Cell("anabolic", r.randint(0, array_size), r.randint(0, array_size))
-    catabolic_cells[f"C_Cell_{i+1}"] = AC.Cell("catabolic", r.randint(0, array_size), r.randint(0, array_size))
+    a_start_row = r.randint(0, array_size-1)
+    a_start_column = r.randint(0, array_size-1)
+    anabolic_cells[f"A_Cell_{i+1}"] = AC.Cell("anabolic", a_start_row, a_start_column)
+    cell_location[a_start_row, a_start_column] = 1
     
+    c_start_row = r.randint(0, array_size-1)
+    c_start_column = r.randint(0, array_size-1)
+    catabolic_cells[f"C_Cell_{i+1}"] = AC.Cell("catabolic", c_start_row, c_start_column)
+    cell_location[c_start_row, c_start_column] = 0
 iterations = 1000
 for t in range(iterations):
     for anabolic in anabolic_cells.keys():
-        row, column = move(anabolic_cells[anabolic], array_size)
-        grid[row, column] += .2 
+        current_position = (anabolic_cells[anabolic].Get_Row(), anabolic_cells[anabolic].Get_Column())
+        cell_location[current_position[0], current_position[1]] = np.nan
+        row, column = move_random(anabolic_cells[anabolic], array_size)
+        cell_location[row, column] = 1
+        if grid[row, column] == 1:
+            pass
+        else:
+            grid[row, column] += .05
     
     for catabolic in catabolic_cells.keys():
-        row, column = move(catabolic_cells[catabolic], array_size)
+        current_position = (catabolic_cells[catabolic].Get_Row(), catabolic_cells[catabolic].Get_Column())
+        cell_location[current_position[0], current_position[1]] = np.nan
+        row, column = move_random(catabolic_cells[catabolic], array_size)
+        cell_location[row, column] = 0
         if grid[row, column] == 0:
             pass
         else:
-            grid[row, column] -= .2
-    heatmap.set_array(grid)
+            grid[row, column] -= .05
+    tissue.set_array(grid)
+    cell.set_array(cell_location)
     
-    plt.pause(0.25)
+    plt.pause(0.05)
     
     
 
